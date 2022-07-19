139d87054248e790098b958068527daf9a0b9258
KevinByrne <kevbee79@gmail.com>
2022-07-19 15:46:42 +0100

    updated controller and form created

--- app.rb
+++ app.rb
@@ -7,7 +7,7 @@ class McAirbnb < Sinatra::Base
   end
 
   get '/' do
-    'This is our landing page.'
+    erb :index
   end
 
   run! if app_file == $0
---
+++ spec/features/Viewing_Index_Page_spec.rb
@@ -0,0 +1,6 @@
+# feature 'Viewing Index Page' do
+#   scenario 'visits Index page' do
+#     visit('/')
+#     expect(page).to have_content "This is our landing page."
+#   end
+# end
