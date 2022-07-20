e6599cb8d811ad1ecd0ad59de338d1b72f66ebd2
olliefreeman94 <oliver.freeman@yahoo.com>
2022-07-20 16:46:40 +0100

    Implement self.book method

--- lib/space.rb
+++ lib/space.rb
@@ -2,10 +2,11 @@ require 'pg'
 
 class Space
   
-  attr_reader :name
+  attr_reader :name, :available
 
-  def initialize(name:)
+  def initialize(name:, available:)
     @name = name
+    @available = available
   end
 
   def self.all
@@ -15,11 +16,23 @@ class Space
       connection = PG.connect(dbname: 'makersbnb')
     end
 
-    spaces = connection.exec("SELECT name FROM spaces")
-    spaces.map { |space| Space.new(name: space['name']) }
+    spaces = connection.exec("SELECT name, available FROM spaces")
+    spaces.map { |space| Space.new(name: space['name'], available: space['available']) }
   end
 
-  def self.book(space)
+  def self.book(space_name)
+    if ENV['ENVIRONMENT'] == 'test'
+      connection = PG.connect(dbname: 'makersbnb_test')
+    else
+      connection = PG.connect(dbname: 'makersbnb')
+    end
+
+    name = space_name.gsub("'", "''")
+    result = connection.exec(
+      "UPDATE spaces SET available = false WHERE name = '#{name}' RETURNING name, available"
+      ).first
+    
+    return Space.new(name: result['name'], available: result['available'])
   end
 
   def self.available
