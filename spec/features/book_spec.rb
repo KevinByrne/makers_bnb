2c64c02fbf79e54ee7fddf4a5ab0b63f1a44c1ac
olliefreeman94 <oliver.freeman@yahoo.com>
2022-07-21 14:58:25 +0100

    Update model and view to support extra table columns

--- spec/features/book_spec.rb
+++ spec/features/book_spec.rb
@@ -1,8 +1,8 @@
 feature 'book a space' do
   scenario 'user can book a space' do
     connection = PG.connect(dbname: 'makersbnb_test')
-    connection.exec("INSERT INTO spaces (name, available) VALUES ('Amy''s space', true);")
-    connection.exec("INSERT INTO spaces (name, available) VALUES ('Kev''s space', true);")
+    connection.exec("INSERT INTO spaces (name, available, space_description, price) VALUES ('Amy''s space', true, 'A lovely flat', 150.50);")
+    connection.exec("INSERT INTO spaces (name, available, space_description, price) VALUES ('Kev''s space', true, 'A lovely house', 200.00);")
     
     visit '/portal'
 
--- spec/features/view_listings_spec.rb
+++ spec/features/view_listings_spec.rb
@@ -1,23 +1,23 @@
 feature 'view listings' do
   scenario 'display listings to user' do
     connection = PG.connect(dbname: 'makersbnb_test')
-    connection.exec("INSERT INTO spaces (name, available) VALUES ('Amy''s space', true);")
-    connection.exec("INSERT INTO spaces (name, available) VALUES ('Kev''s space', true);")
-    connection.exec("INSERT INTO spaces (name, available) VALUES ('Ollie''s space', true);")
+    connection.exec("INSERT INTO spaces (name, available, space_description, price) VALUES ('Amy''s space', true, 'A lovely flat', 150.50);")
+    connection.exec("INSERT INTO spaces (name, available, space_description, price) VALUES ('Kev''s space', true, 'A lovely house', 200.00);")
+    connection.exec("INSERT INTO spaces (name, available, space_description, price) VALUES ('Ollie''s space', true, 'A not so lovely flat', 10.25);")
     
     visit '/portal'
 
-    expect(page).to have_content("Amy's space")
-    expect(page).to have_content("Kev's space")
-    expect(page).to have_content("Ollie's space")
+    expect(page).to have_content("Amy's space - A lovely flat - £150.50 p/n")
+    expect(page).to have_content("Kev's space - A lovely house - £200.00 p/n")
+    expect(page).to have_content("Ollie's space - A not so lovely flat - £10.25 p/n")
   end
 
  
   scenario 'display updated listings to user after booking made' do
     connection = PG.connect(dbname: 'makersbnb_test')
-    connection.exec("INSERT INTO spaces (name, available) VALUES ('Amy''s space', true);")
-    connection.exec("INSERT INTO spaces (name, available) VALUES ('Kev''s space', true);")
-    connection.exec("INSERT INTO spaces (name, available) VALUES ('Ollie''s space', true);")
+    connection.exec("INSERT INTO spaces (name, available, space_description, price) VALUES ('Amy''s space', true, 'A lovely flat', 150.50);")
+    connection.exec("INSERT INTO spaces (name, available, space_description, price) VALUES ('Kev''s space', true, 'A lovely house', 200.00);")
+    connection.exec("INSERT INTO spaces (name, available, space_description, price) VALUES ('Ollie''s space', true, 'A not so lovely flat', 10.25);")
     
     visit '/portal'
     click_button("Book Amy's space")
